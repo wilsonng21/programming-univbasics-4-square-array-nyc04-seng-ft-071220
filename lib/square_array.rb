587c1cc0b6b0d30e30614867ffe45614fa53ea83
Wilson Ng <wilsongch92@gmail.com>
2020-07-16 14:29:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   index = 0
   while array.length.times do
-    new_array.push(array[index] ** 2)
+    new_array.push(array[index]**2)
     index += 1
   end
   new_array
